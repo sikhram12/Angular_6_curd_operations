8c70e57f967d2d676b7f95cd4d483200f04416f8
anil_sikhram <anil.sikhram@maryland.gov>
2019-10-28 23:45:00 -0400

    Field validations

--- src/app/dialogs/add/add.component.ts
+++ src/app/dialogs/add/add.component.ts
@@ -14,66 +14,74 @@ export class AddComponent {
 
   form: FormGroup;
   emp: EmployeeObject;
+  invalidFormFlag = false;
 
   constructor(private readonly cdr: ChangeDetectorRef, public dialogRef: MatDialogRef<AddComponent>, private readonly fb: FormBuilder,
-    @Inject(MAT_DIALOG_DATA) data, 
+    @Inject(MAT_DIALOG_DATA) data,
     public dataService: AppService) {
-      this.emp = data.repository;
-     }
+    this.emp = data.repository;
+    this.form = fb.group({
+      // Adding the "myNum" input to our FormGroup along with its min-max Validators.
+      'phone': ['', [Validators.min(5)]]
+    })
+  }
 
-     ngAfterViewInit() {
-      this.cdr.detectChanges();
+  ngAfterViewInit() {
+    this.cdr.detectChanges();
+  }
+
+  ngOnInit() {
+
+    this.form = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      age: [''],
+      experience: ['', []],
+      phone: ['', []],
+      streetAdress: [''],
+      city: ['', []],
+      state: ['', []],
+      zipcode: [''],
+      workingDays: ['', []],
+      joinDate: ['', []],
+    });
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+  save() {
+    this.validateReqFields();
+    if (!this.invalidFormFlag) {
+    this.emp = new EmployeeObject();
+
+    this.emp.firstName = this.form.get('firstName').value;
+    this.emp.lastName = this.form.get('lastName').value;
+    this.emp.age = this.form.get('age').value;
+    this.emp.experience = this.form.get('experience').value;
+    this.emp.phone = this.form.get('phone').value;
+    this.emp.streetAdress = this.form.get('streetAdress').value;
+    this.emp.city = this.form.get('city').value;
+    this.emp.state = this.form.get('state').value;
+    this.emp.zipcode = this.form.get('zipcode').value;
+    this.emp.workingDays = this.form.get('workingDays').value;
+    // this.emp.joinDate = this.form.get('joinDate').value;
+
+    this.dialogRef.close(this.emp);
     }
 
-    ngOnInit() {
-     
-        this.form = this.fb.group({
-          firstName: ['', []],
-          lastName: ['', []],
-          age: [''],
-          experience: ['', []],
-          phone: ['', []],
-          streetAdress: [''],
-          city: ['', []],
-          state: ['', []],
-          zipcode: [''],
-          workingDays: ['', []],
-          joinDate: ['', []],
-        });
-      }
+  }
 
-      close() {
-        this.dialogRef.close();
-      }
-
-      save() {
-       // this.validateReqFields();
-        // const empObj : EmployeeObject = {
-        //   firstName : this.form.get('firstName').value,
-        //   lastName : this.form.get('lastName').value,
-        //   age : this.form.get('age').value
-        // }; 
-        
-          this.emp = new EmployeeObject();
-         
-          this.emp.firstName = this.form.get('firstName').value;
-          this.emp.lastName = this.form.get('lastName').value;
-          this.emp.age = this.form.get('age').value;
-          this.emp.experience = this.form.get('experience').value;
-          this.emp.phone = this.form.get('phone').value;
-          this.emp.streetAdress = this.form.get('streetAdress').value;
-          this.emp.city = this.form.get('city').value;
-          this.emp.state = this.form.get('state').value;
-          this.emp.zipcode = this.form.get('zipcode').value;
-          this.emp.workingDays = this.form.get('workingDays').value;
-          // this.emp.joinDate = this.form.get('joinDate').value;
-         
-          this.dialogRef.close(this.emp);
-    
-        
-      }
-
+  validateReqFields() {
+    if (this.form.get('firstName').value === "" || this.form.get('lastName').value === "" || this.form.get('experience').value === "") {
+      this.invalidFormFlag = true;
+    } else {
+      this.invalidFormFlag = false;
     }
+  }
+
+}
 
 
 
